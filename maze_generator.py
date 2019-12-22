a2299e238c51958b90374c601d85ee544c064c2d
Jakub Rada <jacub.rada@gmail.com>
2019-12-22 15:37:21 +0100

    maze generating done

--- maze_generator.py
+++ maze_generator.py
@@ -17,6 +17,8 @@ EMPTY = 'EMPTY'
 WALL = 'OBSTACLE'
 GOLD = 'GOLD'
 START = 'START'
+RIGHT_LINE_THRESHOLD = 10
+GOLD_THRESHOLD = 5
 
 def generate_empty(maze_size):
     """Creates an empty maze with start at top-left corner and gold at the opposite corner"""
@@ -77,8 +79,128 @@ def init_maze(rows, cols):
 
 def place_start_gold(layout):
     layout[0][0] = START
-    layout[len(layout) - 3][0] = GOLD
+    gold_position = (len(layout) - 3, 0)
+    if len(layout) == GOLD_THRESHOLD:
+        gold_position = (len(layout) - 2, 0)
+    layout[gold_position[0]][gold_position[1]] = GOLD
+    layout[gold_position[0] - 1][gold_position[1]] = WALL
+    layout[gold_position[0]][gold_position[1] + 1] = WALL
 
+
+def place_right_line(layout):
+    for i in range(1, len(layout) - 1):
+        layout[i][len(layout[0]) - 2] = WALL
+    layout[len(layout) - 1][len(layout[0]) - 3] = WALL
+
+def place_start_path(layout):
+    for i in range(1, len(layout) - 3):
+        layout[i][1] = WALL
+
+def place_gold_path(layout):
+    start = len(layout[0]) - 2
+    if len(layout[0]) > RIGHT_LINE_THRESHOLD:
+        place_right_line(layout)
+        start -= 2
+    for i in range(1, start):
+        layout[len(layout) - 2][i] = WALL
+    for i in range(len(layout) - 3, 0, -1):
+        layout[i][start] = WALL
+
+def place_dead_ends_thin(layout):
+    count = len(layout[0]) - 6
+    position = len(layout[0]) - 4
+    if len(layout[0]) > RIGHT_LINE_THRESHOLD:
+        count -= 2
+        position -= 2
+    count = int(count / 2)
+    for i in range(count, 0, -1):
+        y = 1
+        x = position
+        c = 2
+        if i == count:
+            c = i + 1
+        while layout[y + c][x] != WALL:
+            layout[y][x] = WALL
+            y += 1
+        y -= 1
+        for n in range(i + 1):
+            layout[y][x] = WALL
+            y += 1
+            x -= 1
+        y -= 1
+        while layout[y][x] != WALL:
+            layout[y][x] = WALL
+            x -= 1
+        position -= 2
+
+def diagonalize_up(layout, x, y):
+    while layout[y - 1][x + 1] != WALL:
+        layout[y][x] = WALL
+        y -= 1
+        x += 1
+
+def place_diagonals_thin(layout):
+    y = len(layout) - 3
+    if len(layout) <= GOLD_THRESHOLD:
+        y += 1
+    x = 2
+    while layout[y - 1][x] == WALL:
+        x += 1
+    x += 1
+    while layout[y][x] != WALL:
+        diagonalize_up(layout, x, y)
+        x += 3
+
+def place_dead_ends_thick(layout):
+    count = len(layout[0]) - 4
+    if len(layout[0]) >= RIGHT_LINE_THRESHOLD:
+        count -= 2
+    count = int((count - 1) / 2)
+    x = 3
+    if len(layout[0]) % 2 == 0:
+        x += 1
+    for i in range(count):
+        y = 1
+        while layout[y][x] != WALL:
+            layout[y][x] = WALL
+            y += 1
+        x += 2
+
+def clear_vertical(layout):
+    end = len(layout) - 2
+    start = 1
+    if end <= GOLD_THRESHOLD:
+        end += 1
+        start += 1
+    for y in range(start, end):
+        if layout[y][2] == WALL:
+            layout[y][1] = EMPTY
+
+def clear_horizontal(layout):
+    end = len(layout[0]) - 3
+    if len(layout[0]) >= RIGHT_LINE_THRESHOLD:
+        end -= 2
+    y = len(layout) - 2
+    for x in range(1, end):
+        if layout[y - 1][x] == WALL:
+            layout[y][x] = EMPTY
+
+def place_top_thin(layout):
+    x = 3
+    y = 2
+    while layout[y][x] == EMPTY and layout[y + 1][x] == EMPTY:
+        layout[y][x] = WALL
+        layout[y][x + 1] = EMPTY
+        y += 2
+
+def place_top_thick(layout):
+    if len(layout[0]) % 2 == 0:
+        x = 3
+        y = 2
+        while layout[y][x] == EMPTY and layout[y + 1][x] == EMPTY:
+            layout[y][x] = WALL
+            layout[y][x + 1] = EMPTY
+            y += 2
 
 def generate_maze(maze_size):
     """
@@ -93,27 +215,41 @@ def generate_maze(maze_size):
     cols = maze_size[1]
     layout = init_maze(rows, cols)
     place_start_gold(layout)
-
-
-    return layout
-    # if is_feasible(layout):
-    #     return layout
+    place_gold_path(layout)
+    place_start_path(layout)
+    if rows >= cols:
+        place_dead_ends_thin(layout)
+        if cols > 13:
+            place_diagonals_thin(layout)
+        place_top_thin(layout)
+    else:
+        place_dead_ends_thick(layout)
+        place_top_thick(layout)
+    clear_vertical(layout)
+    clear_horizontal(layout)
+    place_start_gold(layout)
+    if is_feasible(layout):
+        return layout
 
 def print_maze(maze):
     values = {
-        EMPTY: " ",
-        WALL: "#",
+        EMPTY: "·",
+        WALL: "\u2588",
         START: "S",
         GOLD: "G"
     }
-    print("-" * (len(maze[0]) + 2))
+    print("\u250f", end="")
+    print("\u2501" * len(maze[0]), end="")
+    print("\u2513")
     for row in maze:
-        print("|", end="")
+        print("\u2503", end="")
         for col in row:
             print(values[col], end="")
-        print("|")
-    print("-" * (len(maze[0]) + 2))
+        print("\u2503")
+    print("\u2517", end="")
+    print("\u2501" * len(maze[0]), end="")
+    print("\u251b")
     
 
 if __name__ == '__main__':
-    print_maze(generate_maze((10, 10)))
+    print_maze(generate_maze((20, 20)))
